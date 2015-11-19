2cf15dcd884e227d6ef7aecb3195e6835401b418
josephedmunds <jee431@utulsa.edu>
2015-11-19 14:05:49 -0600

    Quadratic Probing finished Fully commented

--- src/Driver.java
+++ src/Driver.java
@@ -5,14 +5,13 @@
 public class Driver {
     public static void main(String[] args) {
         //Array of size 2. Argument 1: Load factor. Argument 2: Probe Type
-        //int[] arguments = Utilities.acceptInput();
+        int[] arguments = Utilities.acceptInput();
 
-        //Hash hash = new Hash(arguments[0], arguments[1], arguments[2]);
+        Hash hash = new Hash(arguments[0], arguments[1], arguments[2]);
 
-        //FOR TEST PURPOSES
-        Hash hash = new Hash(1019, 80, 1);
-
-        hash.generateStats(0);
-
+        if (arguments[2] == 0)
+            hash.generateStatsLinear(0);
+        else
+            hash.generateStatsQuadratic(1);
     }
 }
--- src/Hash.java
+++ src/Hash.java
@@ -8,10 +8,10 @@ public class Hash {
     private double load;
 
     /**
-     * Hash Constructor
-     * @param size
-     * @param loadFactor
-     * @param probeType
+     * Constructor for the Hash Table
+     * @param size The size of the hash table
+     * @param loadFactor The load factor of the hash table
+     * @param probeType The probe type, either linear or quadratic for this project
      */
     public Hash(int size, int loadFactor, int probeType) {
         this.probe = probeType;
@@ -27,6 +27,7 @@ public class Hash {
     /**
      * Fills the Hash Table
      * @param probeType Whether to fill the table through linear probing or quadratic probing
+     * @param fillSize The amount of the table that will be filled, based on the load factor
      */
     public void fillTable(int probeType, int fillSize) {
         int index;
@@ -45,7 +46,7 @@ public class Hash {
                     }
                     table[index] = currKey;
                 } else if (probeType == 1) {
-                    index = quadraticProbe(index, table, currKey);
+                    index = quadraticProbe(index, table, currKey)[0];
                     table[index] = currKey;
                 }
             }
@@ -54,8 +55,8 @@ public class Hash {
 
     /**
      * Instructions for linear probing
-     * @param currIndex
-     * @return
+     * @param currIndex The index currently being inspected
+     * @return The next index to be inspected
      */
     public int linearProbe(int currIndex) {
         currIndex++;
@@ -64,36 +65,48 @@ public class Hash {
         return currIndex;
     }
 
-
-    public int quadraticProbe(int startIndex, int[] hashTable, int value) {
+    /**
+     * Instructions for quadratic probing
+     * @param startIndex The index the quadratic probe will start from
+     * @param hashTable The hash table
+     * @param value The value being searched for in the table
+     * @return An array of the next index to test and the variance use to find that index
+     */
+    public int[] quadraticProbe(int startIndex, int[] hashTable, int value) {
+        int[] info = new int[2];
         int variance = 1;
+        int diff = 0;
 
         while (hashTable[startIndex] != -5) {
             if (hashTable[startIndex] == value){
                 break; //Found
             }
             if (variance > 1) {
-                startIndex += (int)Math.pow((double)variance, 2.0) % 1019;
+                diff = (int)Math.pow((double)variance, 2.0) % 1019;
+                startIndex += diff;
                 variance *= -1;
             }
             else {
-                startIndex -= (int)Math.pow((double)variance, 2.0) % 1019;
+                diff = (int)Math.pow((double)variance, 2.0) % 1019;
+                startIndex -= diff;
                 while (startIndex < 0)
                     startIndex += 1019;
                 variance -= 1;
             }
         }
-        return startIndex;
+        info[0] = startIndex;
+        info[1] = variance;
+        return info;
     }
 
     /**
-     *
-     * @param hashTable
-     * @param value
-     * @param key
-     * @param probeType
-     * @param data
-     * @return
+     * A method to search the hash table to find a value
+     * @param hashTable The hash table being searched
+     * @param value The value being looked for in the table
+     * @param key The key where the value is being looked for
+     * @param probeType The type of probing that will be used, if necessary, to search for the value
+     * @param data An array that holds the number of probes used to find the value, and whether or not the search was successful
+     * @return The data array
      */
     public int[] searchTable(int[] hashTable, int value, int key, int probeType, int[] data) {
         data[0]++; //Increment the number of probes every time this method is called. Will stop accumulating when the loops break.
@@ -114,7 +127,7 @@ public class Hash {
                     data = searchTable(hashTable, value, linearProbe(key), type, data);
                 }
                 else if (type == 1) {
-                    data = searchTable(hashTable, value, quadraticProbe(key, hashTable, value), type, data);
+                    data = searchTable(hashTable, value, quadraticProbe(key, hashTable, value)[0], type, data);
                 }
         }
 
@@ -122,10 +135,10 @@ public class Hash {
     }
 
     /**
-     * Finds the statistics and outputs them in an easy to read format
-     * @param probeType
+     * Finds the statistics of a linear probe and outputs them in an easy to read format
+     * @param probeType The type of probing being used
      */
-    public void generateStats(int probeType) {
+    public void generateStatsLinear(int probeType) {
         int[] data = new int[2];
         double[] successStats = new double[2];
         double[] failStats = new double[2];
@@ -149,5 +162,42 @@ public class Hash {
         double successAvg = (successStats[1]) / successStats[0];
         System.out.printf("Average probes per success: %f\n", successAvg);
 
+    }
+
+    /**
+     * Finds the statistics of a quadratic probe and outputs them in an easy to read format
+     * @param probeType The type of probing being used
+     */
+    public void generateStatsQuadratic(int probeType) {
+        int[] data = new int[2];
+        double[] successStats = new double[2];
+        double[] failStats = new double[2];
+        for (int i = 1; i <= 10000; i++) {
+            data[0] = 0;
+            data = searchTable(table, i, i % 1019, probeType, data);
+
+            if (data[1] == 1) {
+                successStats[0]++; //increment number of successful searches
+                int hold = quadraticProbe(i % 1019, table, i)[1];
+                if (hold <= 0)
+                    successStats[1] += -hold*2 + 1;
+                else
+                    successStats[1] += hold*2;
+            }
+            else {
+                failStats[0]++; //increments the number of failed searches
+                int hold = quadraticProbe(i % 1019, table, i)[1];
+                if (hold <= 0)
+                    failStats[1] += -hold*2 + 1;
+                else
+                    failStats[1] += hold*2;
+            }
+        }
+
+        double failAvg = failStats[1] / failStats[0];
+        System.out.printf("Average probes per failure: %f\n", failAvg);
+
+        double successAvg = (successStats[1]) / successStats[0];
+        System.out.printf("Average probes per success: %f\n", successAvg);
     }
 }
